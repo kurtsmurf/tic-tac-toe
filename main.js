1af97254ab8e9f86af23d16772284168cf31fd01
Chris Smith <chrisronsmith@gmail.com>
2020-08-24 08:12:33 -0400

    Make board

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 import * as preact from 'preact'
 import * as hooks from 'preact/hooks'
 
-const Thing = () => {
-    const [message, setMessage] = hooks.useState('oNo')
+const Cell = (value, position) => (
+  preact.h(
+    'div',
+    {
+      id: position,
+      className: `cell pos-${position}`
+    },
+    value
+  )
+)
 
-    return preact.h(
-        'div',
-        { onClick: () => setMessage(message + "!") },
-        message
-    )
+const Board = ({ board }) => {
+  console.log(board)
+
+  return preact.h(
+    'div',
+    { className: 'board' },
+    board.map(Cell)
+  )
+}
+
+const emptyBoard = new Array(9).fill("O")
+
+const App = () => {
+  const [board, setBoard] = hooks.useState(emptyBoard)
+
+  return preact.h(Board, { board })
 }
 
 preact.render(
-    preact.h(Thing),
-    document.getElementById('app')
+  preact.h(App),
+  document.getElementById('app')
 )
