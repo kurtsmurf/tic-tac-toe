f7f0562996d85df66913c544b4b73b488570d6ad
Chris Smith <chrisronsmith@gmail.com>
2020-08-27 19:39:52 -0400

    Highlight wins

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 import * as preact from 'preact'
 import * as hooks from 'preact/hooks'
 
-const Cell = ({ value, position, onClick }) => (
+const Cell = ({ value, position, onClick, highlight }) => (
   preact.h(
     'div',
     {
       id: position,
-      className: `cell pos-${position}`,
+      className: `cell pos-${position} ${highlight ? 'highlight' : ''}`,
       onClick
     },
     value
   )
 )
 
-const Board = ({ board, play }) => (
+const Board = ({ board, play, winningCombo }) => (
   preact.h(
     'div',
     {
@@ -24,7 +24,8 @@ const Board = ({ board, play }) => (
       {
         value,
         position,
-        onClick: value === "" ? () => play(position) : null
+        onClick: value === "" ? () => play(position) : null,
+        highlight: winningCombo && winningCombo.includes(position)
       }
     ))
   )
@@ -49,18 +50,18 @@ const gameStates = {
 
 const emptyBoard = new Array(9).fill("")
 
-const winningCombos = [
-  [0, 1, 2], [3, 4, 5], [6, 7, 8],
-  [0, 3, 6], [1, 4, 7], [2, 5, 8],
-  [0, 4, 8], [2, 4, 6]
-]
+const getWinningCombo = (player, board) => {
+  const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]  
 
-const didWin = (player, board) => {
   const playerHasCombo = combo => combo
     .map(position => board[position])
     .every(value => value === player)
 
-  return winningCombos.some(playerHasCombo)
+  return winningCombos.filter(playerHasCombo)[0]
 }
 
 const isDraw = board => !board.includes("")
@@ -69,6 +70,7 @@ const App = () => {
   const [board, setBoard] = hooks.useState(emptyBoard)
   const [player, setPlayer] = hooks.useState("X")
   const [gameState, setGameState] = hooks.useState(gameStates.playing)
+  const [winningCombo, setWinningCombo] = hooks.useState(null)
 
   const togglePlayer = () => setPlayer(player === "X" ? "O" : "X")
 
@@ -84,9 +86,11 @@ const App = () => {
     if (gameState !== gameStates.playing) return
 
     const nextBoard = getNextBoard(position)
+    const win = getWinningCombo(player, nextBoard)
 
-    if (didWin(player, nextBoard)) {
+    if (win) {
       setGameState(gameStates.won)
+      setWinningCombo(win)
     } else if (isDraw(nextBoard)) {
       setGameState(gameStates.draw)
     } else {
@@ -99,7 +103,7 @@ const App = () => {
   return preact.h(
     preact.Fragment,
     {},
-    preact.h(Board, { board, play }),
+    preact.h(Board, { board, play, winningCombo }),
     preact.h(Prompt, { gameState, player })
   )
 }
