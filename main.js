ace7782e1c6b34a2ea375fe28a745bd62cba84ae
Chris Smith <chrisronsmith@gmail.com>
2020-08-27 14:51:32 -0400

    Add prompt, handle winning and draws

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as preact from 'preact'
 import * as hooks from 'preact/hooks'
 
-const Cell = ({value, position, onClick}) => (
+const Cell = ({ value, position, onClick }) => (
   preact.h(
     'div',
     {
@@ -30,12 +30,29 @@ const Board = ({ board, play }) => (
   )
 )
 
+const Prompt = ({ gameState, player }) => {
+  const prompt =
+    gameState === gameStates.playing
+      ? `Player ${player}'s turn.`
+      : gameState === gameStates.won
+        ? `Player ${player} wins!`
+        : `It's a draw :/`
+
+  return preact.h('h1', {}, prompt)
+}
+
+const gameStates = {
+  playing: 0,
+  won: 1,
+  draw: 2
+}
+
 const emptyBoard = new Array(9).fill("")
 
 const winningCombos = [
-  [0,1,2], [3,4,5], [6,7,8],
-  [0,3,6], [1,4,7], [2,5,8],
-  [0,4,8], [2,4,6]
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
 ]
 
 const didWin = (player, board) => {
@@ -46,9 +63,12 @@ const didWin = (player, board) => {
   return winningCombos.some(playerHasCombo)
 }
 
+const isDraw = board => !board.includes("")
+
 const App = () => {
   const [board, setBoard] = hooks.useState(emptyBoard)
   const [player, setPlayer] = hooks.useState("X")
+  const [gameState, setGameState] = hooks.useState(gameStates.playing)
 
   const togglePlayer = () => setPlayer(player === "X" ? "O" : "X")
 
@@ -61,10 +81,14 @@ const App = () => {
   }
 
   const play = position => {
+    if (gameState !== gameStates.playing) return
+
     const nextBoard = getNextBoard(position)
 
     if (didWin(player, nextBoard)) {
-      console.log(`Player ${player} wins!`)
+      setGameState(gameStates.won)
+    } else if (isDraw(nextBoard)) {
+      setGameState(gameStates.draw)
     } else {
       togglePlayer()
     }
@@ -72,7 +96,12 @@ const App = () => {
     setBoard(nextBoard)
   }
 
-  return preact.h(Board, { board, play })
+  return preact.h(
+    preact.Fragment,
+    {},
+    preact.h(Board, { board, play }),
+    preact.h(Prompt, { gameState, player })
+  )
 }
 
 preact.render(
